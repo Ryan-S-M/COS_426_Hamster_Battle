394a5d9fe5d5a71f81e70139e28211e16d8ab9c8
Jeremy Kiil <jkiil@princeton.edu>
2023-12-12 21:18:41 -0500

    HamsterController done?

--- src/components/controls/HamsterController.js
+++ src/components/controls/HamsterController.js
@@ -1,16 +1,30 @@
-import { HamsterSphere } from 'objects';
-import { Vector3 } from 'three';
+import { Box, HamsterSphere } from 'objects';
+import Vector3 from 'three';
+import SeedScene from '../scenes/SeedScene';
 
 class HamsterController {
     /**
      * @param {!HamsterSphere} playerSphere
      * @param {!Array<!HamsterSphere>} NPCSpheres
      * @param {number} difficulty
+     * @param {!SeedScene} scene
      */
-    constructor(playerSphere, NPCSpheres, difficulty) {
+    constructor(playerSphere, NPCSpheres, difficulty, scene) {
         this.playerSphere = playerSphere;
         this.NPCSpheres = NPCSpheres;
         this.difficulty = difficulty;
+        this.scene = scene;
+        let box_params = {
+            l: scene.box.geometry.parameters.depth,
+            w: scene.box.geometry.parameters.width,
+            h: scene.box.geometry.parameters.height
+        };
+        this.boundaries = {
+            x_min: -box_params.w / 2,
+            x_max: box_params.w / 2,
+            z_min: -box_params.l / 2,
+            z_max: box_params.l / 2
+        }
     }
 
     /**
@@ -21,10 +35,41 @@ class HamsterController {
         if (hs.index == this.playerSphere.index) {
             return null;
         }
-        let vec_btw_centers = this.playerSphere.position.clone().sub(hs.position);
-        let best_direction = (new Vector3(vec_btw_centers.x, 0, vec_btw_centers.z)).normalize();
-        
 
+        let vec_btw_centers = this.playerSphere.position.clone().sub(hs.position);
+        let best_direction = new Vector3(vec_btw_centers.x, 0, vec_btw_centers.z);
+        let angle = hs.direction.angleTo(best_direction);
+
+        let EPS = 1;
+        let near_edge = false;
+        if (hs.position.x < this.boundaries.x_min + EPS ||
+            hs.position.x > this.boundaries.x_max - EPS ||
+            hs.position.z < this.boundaries.z_min + EPS ||
+            hs.position.z > this.boundaries.z_max - EPS) {
+            near_edge = true;
+        }
+
+        let best_choice;
+        if (near_edge) {
+            if (angle < Math.PI) {
+                best_choice = "left";
+            } else {
+                best_choice = "right";
+            }
+        }
+        if (Math.abs(angle) < Math.PI / 6) {
+            best_choice = "forward";
+        } else if (angle < Math.PI) {
+            best_choice = "left";
+        } else {
+            best_choice = "right";
+        }
+
+        if (!near_edge && Math.random() < 0.2) {
+            return "nothing";
+        } else {
+            return best_choice;
+        }
     }
 }
 
